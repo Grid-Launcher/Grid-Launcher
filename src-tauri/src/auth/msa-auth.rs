759937b13f97921bfc1dbb281a09e8e92b9ece50
code-onigiri <shiba.jp@outlook.com>
2024-07-02 06:27:27 +0900

    add

--- src-tauri/src/auth/msa-auth.rs
+++ src-tauri/src/auth/msa-auth.rs
@@ -5,8 +5,18 @@ use oauth2::reqwest::async_http_client;
 use oauth2::{AuthUrl, ClientId, DeviceAuthorizationUrl, Scope, TokenResponse, TokenUrl};
 use reqwest::Client;
 
+const DEVICE_CODE_URL: &str = "https://login.microsoftonline.com/consumers/oauth2/v2.0/devicecode";
+const MSA_AUTHORIZE_URL: &str = "https://login.microsoftonline.com/consumers/oauth2/v2.0/authorize";
+const MSA_TOKEN_URL: &str = "https://login.microsoftonline.com/common/oauth2/v2.0/token";
 
-async fn get_msa_auth() -> Result<(), Box<dyn std::error::Error>> {
 
+async fn get_msa_auth(client_id) -> Result<(), Box<dyn std::error::Error>> {
+    let client = BasicClient::new(
+        ClientId::new(client_id),
+        None,
+        AuthUrl::new(MSA_AUTHORIZE_URL.to_string())?,
+        Some(TokenUrl::new(MSA_TOKEN_URL.to_string())?),
+    )
+    .set_device_authorization_url(DeviceAuthorizationUrl::new(DEVICE_CODE_URL.to_string())?);
 }
 
